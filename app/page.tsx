3e671107a5d71afe012889e90a9139fa4aa74e9f
ryo7400s-del <ryo7400.s@gmail.com>
2026-07-16 20:09:32 +0900

    debug: show auth state on home page

--- app/page.tsx
+++ app/page.tsx
@@ -93,6 +93,9 @@ export default function Home() {
         </div>
       </div>
 
+      <pre style={{ fontSize: 10, background: "#eee", padding: 8, marginBottom: 10 }}>
+        {JSON.stringify({ restoring, hasLoginResult: !!loginResult, hasWallet: !!wallet, wallet }, null, 2)}
+      </pre>
       {restoring ? null : !loginResult ? (
         <div style={{ textAlign: "center", marginTop: 60 }}>
           <p style={{ fontSize: 13, color: "#6B7688", marginBottom: 16 }}>
