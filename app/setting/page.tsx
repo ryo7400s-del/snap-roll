8563ba48629a90b16d4fa2706d547ab15de355f1
ryo7400s-del <ryo7400.s@gmail.com>
2026-08-20 14:36:26 +0900

    Point frontend at redeployed PayrollFactory (EscrowVault support)
    New factory: 0xD1bc85bFED447d2EfF2AF484592a1f5eFF555bd4
    New EscrowVaultFactory: 0x993D47A4Dd0be69B91B17DEC3062784c6187494b

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -5,7 +5,7 @@ import Papa from "papaparse";
 import { useCircleAuth } from "../components/useCircleAuth";
 import { usePasskey } from "../components/usePasskey";
 
-const FACTORY_ADDRESS = "0x4D91A08cbfa57BF8AD62262Ccd6a0943Ce621352";
+const FACTORY_ADDRESS = "0xD1bc85bFED447d2EfF2AF484592a1f5eFF555bd4";
 
 // 一度デプロイしたPaymentSchedulerV2のアドレスを保持する場所。
 // 本来はユーザーごとにSchedulerRegistry等から取得すべきだが、
